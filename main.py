58968a3aa19916a8ae80b662b7cff6fa35f1064b
Shaken Dan <shd.cs.ch@gmail.com>
2023-05-12 15:43:20 +0600

    image is saved in files directory

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/sunny.jpg'
+        # Get users string input from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        # Download the image
+        req = requests.get(image_link)
+        imagepath = 'files/image.jpg'
+        with open(imagepath, 'wb') as file:
+            file.write(req.content)
+        # Set the image in the Image widgets
+        self.manager.current_screen.ids.img.source = imagepath
 
 
 class RootWidget(ScreenManager):
